9e647d878b925232aa8760940bc9cf2a3f1e6e7e
Levan Ilashvili <levan@ilashvili.me>
2025-09-15 14:47:18 +0200

    setup: create next.js app with tailwind and typescript

---
+++ apps/web/next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['@tortuga/hedera', '@tortuga/database'],
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000'],
+    },
+  },
+  env: {
+    NEXT_PUBLIC_HEDERA_NETWORK: process.env.NEXT_PUBLIC_HEDERA_NETWORK,
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
+};
+
+module.exports = nextConfig;
