a160379362303af126a94636bca1b74e684da1bc
Levan Ilashvili <levan@ilashvili.me>
2025-09-21 18:47:33 +0200

    chore: align package versions and turbo config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   transpilePackages: ['@tortuga/hedera', '@tortuga/database'],
   experimental: {
     serverActions: {
